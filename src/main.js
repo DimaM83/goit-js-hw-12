6adfae09478463dba624f082c28b155b177f9773
DimaM83 <mdv16.01.83@gmail.com>
2025-04-09 02:27:01 +0200

    refactor: async/await, UX improvements

--- src/main.js
+++ src/main.js
@@ -36,113 +36,133 @@ async function handleSearch(event) {
   photoGallery.innerHTML = '';
   loader.style.borderColor = 'black';
   loader.style.borderBottomColor = 'transparent';
-  searchWord = event.currentTarget.elements.inputSearch.value;
+
+  searchWord = event.currentTarget.elements.inputSearch.value.trim();
+
+  if (searchWord === '') {
+    iziToast.show({
+      title: 'Oops!',
+      titleColor: 'white',
+      message: 'Please enter a valid search query!',
+      messageColor: 'white',
+      color: 'red',
+      position: 'topCenter',
+      timeout: 5000,
+    });
+    return;
+  }
+
   page = 1;
 
-  doFetch(searchWord, page)
-    .then(data => {
-      if (data.total == 0) {
-        iziToast.show({
-          title: 'Ops.',
-          titleColor: 'white',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          messageColor: 'white',
-          color: 'red',
-          position: 'topCenter',
-          timeout: '5000',
-        });
-        return;
-      } else {
-        buttonMore.hidden = false;
-        photoGallery.insertAdjacentHTML('beforeend', makeGallery(data.data));
-        book.refresh();
-        event.target.reset();
-        page = page + 1;
-        pageLimit = Math.floor(data.data.totalHits / 15);
+  try {
+    const data = await doFetch(searchWord, page);
 
-        if (page == pageLimit) {
-          iziToast.show({
-            titleColor: 'white',
-            message: `We're sorry, but you've reached the end of search results.!`,
-            messageColor: 'white',
-            color: 'blue',
-            position: 'topCenter',
-            timeout: '5000',
-          });
-          buttonMore.hidden = true;
-        }
-      }
-    })
-    .catch(error => {
+    if (data.total === 0) {
       iziToast.show({
-        title: 'Ops.',
+        title: 'Oops!',
         titleColor: 'white',
-        message: error,
+        message: 'No images found. Try another query.',
         messageColor: 'white',
         color: 'red',
         position: 'topCenter',
-        timeout: '5000',
+        timeout: 5000,
       });
-    })
-    .finally(() => {
-      loader.style.borderColor = 'white';
-      loader.style.borderBottomColor = 'transparent';
+      return;
+    }
+
+    buttonMore.hidden = false;
+    photoGallery.insertAdjacentHTML('beforeend', makeGallery(data.data));
+    book.refresh();
+    event.target.reset();
+    page += 1;
+    pageLimit = Math.floor(data.data.totalHits / 15);
+
+    if (page === pageLimit) {
+      iziToast.show({
+        titleColor: 'white',
+        message: `You've reached the end of search results.`,
+        messageColor: 'white',
+        color: 'blue',
+        position: 'topCenter',
+        timeout: 5000,
+      });
+      buttonMore.hidden = true;
+    }
+  } catch (error) {
+    buttonMore.hidden = true;
+    iziToast.show({
+      title: 'Error!',
+      titleColor: 'white',
+      message: error.message,
+      messageColor: 'white',
+      color: 'red',
+      position: 'topCenter',
+      timeout: 5000,
     });
+  } finally {
+    loader.style.borderColor = 'white';
+    loader.style.borderBottomColor = 'transparent';
+  }
 }
 
-async function searchMore(event) {
+async function searchMore() {
   buttonMore.hidden = true;
   loader.style.borderColor = 'black';
   loader.style.borderBottomColor = 'transparent';
 
-  doFetch(searchWord, page)
-    .then(data => {
-      console.log(data.data);
+  try {
+    const data = await doFetch(searchWord, page);
 
-      if (data.total == 0) {
-        iziToast.show({
-          title: 'Ops.',
-          titleColor: 'white',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          messageColor: 'white',
-          color: 'red',
-          position: 'topCenter',
-          timeout: '5000',
-        });
-        return;
-      } else {
-        buttonMore.hidden = false;
-        photoGallery.insertAdjacentHTML('beforeend', makeGallery(data.data));
-        book.refresh();
-        page = page + 1;
-        if (page > pageLimit) {
-          iziToast.show({
-            titleColor: 'white',
-            message: `We're sorry, but you've reached the end of search results.!`,
-            messageColor: 'white',
-            color: 'blue',
-            position: 'topCenter',
-            timeout: '5000',
-          });
-          buttonMore.hidden = true;
-        }
-      }
-    })
-    .catch(error => {
+    if (data.total === 0) {
       iziToast.show({
-        title: 'Ops.',
+        title: 'Oops!',
         titleColor: 'white',
-        message: error,
+        message: 'No more images found.',
         messageColor: 'white',
         color: 'red',
         position: 'topCenter',
-        timeout: '5000',
+        timeout: 5000,
       });
-    })
-    .finally(() => {
-      loader.style.borderColor = 'white';
-      loader.style.borderBottomColor = 'transparent';
+      return;
+    }
+
+    photoGallery.insertAdjacentHTML('beforeend', makeGallery(data.data));
+    book.refresh();
+    page += 1;
+
+    const { height: cardHeight } =
+      photoGallery.firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
     });
+
+    if (page > pageLimit) {
+      iziToast.show({
+        titleColor: 'white',
+        message: `You've reached the end of search results.`,
+        messageColor: 'white',
+        color: 'blue',
+        position: 'topCenter',
+        timeout: 5000,
+      });
+      buttonMore.hidden = true;
+    } else {
+      buttonMore.hidden = false;
+    }
+  } catch (error) {
+    buttonMore.hidden = true;
+    iziToast.show({
+      title: 'Error!',
+      titleColor: 'white',
+      message: error.message,
+      messageColor: 'white',
+      color: 'red',
+      position: 'topCenter',
+      timeout: 5000,
+    });
+  } finally {
+    loader.style.borderColor = 'white';
+    loader.style.borderBottomColor = 'transparent';
+  }
 }
